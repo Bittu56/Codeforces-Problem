037dfaee614162351fbb42f16aa0e75684df1db1
Bittu Pandey <bittu.pandey56@yahoo.com>
2022-03-23 08:00:14 +0530

    Codeforces DIv 2 problem

---
+++ Program1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<stack>
+#include<algorithm>
+#include<functional>
+
+int solve(int m)
+{
+int n;
+cin>>n;
+vector<int>a(n);
+for(int i=0;i<n;i++)
+{
+cin>>a[i];
+}
+sort(a.begin(),a.end());
+cout<<a[n-1]+a[n-2]<<endl;
+return 0;
+}
+
+int main()
+{
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+   
+    int tc;
+    cin>>tc;
+    for(int tt=1;tt<=tc;tt++)
+    {
+        solve(tt);
+    }
+    return 0;
+}
